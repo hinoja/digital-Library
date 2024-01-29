cdb047c497d306b7b805a512aa6d8ead710934c5
hinoja <hinoja2@gmail.com>
2024-01-29 09:25:54 +0100

    filament installed with successful

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Filament\PanelProvider;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
@@ -167,6 +168,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
